f9a15c075ed409b50e9a7a5523ef7d492e1aa141
Amita <sardana.amita2@gmail.com>
2020-11-17 18:54:19 +0530

    Update user and article association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  belongs_to :user, foreign_key: :author_id
+  belongs_to :author, class_name: :User
   has_many :votes
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
