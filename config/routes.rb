7f96080c9f349dfd7fb3b238df529f8af1184b21
Amita <sardana.amita2@gmail.com>
2020-11-17 19:23:18 +0530

    Add article controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+  resources :articles do
+    resources :votes
+  end
+  resources :categories
+
+  resource :session, only: [:new, :create, :destroy]
+  get 'signin' => 'sessions#new'
+
+  resources :users
+  get 'signup' => 'users#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/articles_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do
+
+end
