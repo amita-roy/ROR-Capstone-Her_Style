dd30fda29e74420626594ab3711459317b1a8875
Amita <sardana.amita2@gmail.com>
2020-11-17 18:43:14 +0530

    Add association test for category. categorization and vote

--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Categorization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it 'should belongs to an article' do
+      t = Categorization.reflect_on_association(:article)
+      expect(t.macro).to eq(:belongs_to)
+    end
+    it 'should belongs to a category' do
+      t = Categorization.reflect_on_association(:category)
+      expect(t.macro).to eq(:belongs_to)
+    end
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it 'should have many articles through categorization' do
+      t = Category.reflect_on_association(:articles)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
 end
