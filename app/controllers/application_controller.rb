ffaaf7c3bd73d38b172ca56b43d96c3784f661fc
Amita <sardana.amita2@gmail.com>
2020-11-18 14:01:11 +0530

    Remove linting errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def require_signin
-    unless current_user
-      session[:intended_url] = request.url
-      redirect_to signin_path, alert: 'Please sigin first !'
-    end
+    return if current_user
+
+    session[:intended_url] = request.url
+    redirect_to signin_path, alert: 'Please sigin first !'
   end
 
   def current_user?(user)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
