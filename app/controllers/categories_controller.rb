f35f2f749e5fcf222e5491562f1546c97d48e5bb
Amita <sardana.amita2@gmail.com>
2020-11-20 12:21:21 +0530

    Show article as per their category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,35 +1,32 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
 
-    def index
-        @categories = Category.all
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def new
-        @category = Category.new
-    end
+  def show
+    @category = Category.find_by!(slug: params[:id])
+  end
 
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-            redirect_to root_path
-        else
-            render :new
-        end
-    end
+  private
 
-    def show
-        @category = Category.find_by!(slug: params[:id])
-    end
+  def set_category
+    @category = Category.find_by!(slug: params[:id])
+  end
 
-
-    private 
-    def set_category
-        @category = Category.find_by!(slug: params[:id])
-    end
-
-    def category_params
-        params.require(:category).permit(:name, :priority)
-    end
-
-
+  def category_params
+    params.require(:category).permit(:name, :priority)
+  end
 end
