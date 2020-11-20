cdfe80c84b89fac47c5c8f719a49932b60367199
Amita <sardana.amita2@gmail.com>
2020-11-20 15:05:58 +0530

    Add js for not to reload page while voting

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -5,9 +5,9 @@ module VotesHelper
     output = ''
 
     if user && user.votes?(article)
-      output << link_to('Remove vote', article_vote_path(article), method: :delete)
+      output << link_to('Remove vote', article_vote_path(article), method: :delete, remote: true)
     else
-      output << link_to('Vote', article_vote_path(article), method: :post)
+      output << link_to('Vote', article_vote_path(article), method: :post, remote: true)
     end
     output.html_safe
   end
