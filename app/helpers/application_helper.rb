b800390fb7109ee9449e2a70f73c76fe86fb7823
Amita <sardana.amita2@gmail.com>
2020-11-20 20:43:34 +0530

    Update category show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,15 @@ module ApplicationHelper
 
     routes
   end
+
+  def vote?(user, article)
+    output = ''
+
+    if user && user.votes?(article)
+      output << link_to('Remove vote', article_vote_path(article), method: :delete, remote: true)
+    else
+      output << link_to('Vote', article_vote_path(article), method: :post, remote: true)
+    end
+    output.html_safe
+  end
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,14 +1,3 @@
 module VotesHelper
   private
-
-  def vote?(user, article)
-    output = ''
-
-    if user && user.votes?(article)
-      output << link_to('Remove vote', article_vote_path(article), method: :delete, remote: true)
-    else
-      output << link_to('Vote', article_vote_path(article), method: :post, remote: true)
-    end
-    output.html_safe
-  end
 end
