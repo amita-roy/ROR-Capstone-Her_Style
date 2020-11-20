2690fcde649d710c2c69e8a3a9fc4f895a1dd275
Amita <sardana.amita2@gmail.com>
2020-11-20 17:49:47 +0530

    Update database setup with on_delete: :cascade

--- db/migrate/20201117105801_create_categorizations.rb
+++ db/migrate/20201117105801_create_categorizations.rb
@@ -1,8 +1,8 @@
 class CreateCategorizations < ActiveRecord::Migration[6.1]
   def change
     create_table :categorizations do |t|
-      t.references :article, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true, on_delete: :cascade
+      t.references :category, null: false, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
