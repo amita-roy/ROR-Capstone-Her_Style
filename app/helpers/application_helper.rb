8a1c6a8e74f4b2cb8e3a0bb322ea220d079844c4
Amita <sardana.amita2@gmail.com>
2020-11-22 14:57:44 +0530

    Add all the fonts and vote logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,10 @@ module ApplicationHelper
   def vote?(user, article)
     output = ''
 
-    if user && user.votes?(article)
-      output << link_to('Remove vote', article_vote_path(article), method: :delete, remote: true)
+    if user&.votes?(article)
+      output << link_to(my_icon('filled-vote', class: 'vote-icons'), article_vote_path(article), method: :delete, remote: true)
     else
-      output << link_to('Vote', article_vote_path(article), method: :post, remote: true)
+      output << link_to(my_icon('unfilled-vote', class: 'vote-icons'), article_vote_path(article), method: :post, remote: true)
     end
     output.html_safe
   end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,7 @@ module HerStyle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
