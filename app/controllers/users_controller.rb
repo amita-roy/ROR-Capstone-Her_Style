082de6346bfc73d1a78b9334be05f3abb884d153
Amita <sardana.amita2@gmail.com>
2020-11-23 21:11:58 +0530

    Update data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user, notice: 'Thanks for successfully signed up!'
+      redirect_to root_path, notice: 'Thanks for successfully signed up!'
     else
       render :new
     end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -13,6 +13,8 @@ module CategoriesHelper
   end
 
   def article_image(article)
-    rails_blob_url(article.image)
+    if article
+      rails_blob_url(article.image)
+    end
   end
 end
