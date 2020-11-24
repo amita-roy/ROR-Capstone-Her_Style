94a076d5786d7a3e3b7a3b9ec07feb167dc55095
Amita <sardana.amita2@gmail.com>
2020-11-24 16:22:15 +0530

    Update the validation for user email's length and article title's length

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :voted_articles, through: :votes, source: :article
 
   validates :name, presence: true, length: { minimum: 3, maximum: 10 }, uniqueness: true
-  validates :email, presence: true, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: true
+  validates :email, presence: true, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: true, length: { minimum: 10, maximum: 30 }
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 
   def votes?(article)
