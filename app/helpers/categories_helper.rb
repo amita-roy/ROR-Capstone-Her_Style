e5c4dca5d1eaae793e998c349cf5b4124487dba2
Amita <sardana.amita2@gmail.com>
2020-11-24 20:31:52 +0530

    Add cloudinary for image and video save

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,21 +1,20 @@
 module CategoriesHelper
   private
 
-  def cat_articles(category)
-    category.articles.map do |article|
-      article
+  def category_background(category)
+    if category.most_recent_article
+      cloudinary_url(category.most_recent_article.image.key, width: 400, crop: :scale)
+    else
+      'https://images.unsplash.com/photo-1573164574001-518958d9baa2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
     end
   end
 
-  def category_background(category)
-    article = category.most_recent_article
-    article ? rails_blob_url(article.image) : ''
-  end
-
-  def article_image(article)
-    return unless article
-
-    rails_blob_url(article.image)
+  def article_image(article, width = 400)
+    if article
+      cloudinary_url(article.image.key, width: width, crop: :scale)
+    else
+      'https://images.unsplash.com/photo-1573164574001-518958d9baa2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    end
   end
 
   def category_article1(articles, count)
